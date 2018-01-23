0184fe0a727bbca9154d4f6cbf7b581e2566fc9d
azarovalex <azarov.333@gmail.com>
2018-01-24 01:47:59 +0300

    Side menu with gestures recognisers added.

---
+++ azChat-iOS/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  azChat-iOS
+//
+//  Created by Alex Azarov on 24/01/2018.
+//  Copyright © 2018 Alex Azarov. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
---
+++ azChat-iOS/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVC.swift
+//  azChat-iOS
+//
+//  Created by Alex Azarov on 24/01/2018.
+//  Copyright © 2018 Alex Azarov. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside )
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    
+}
