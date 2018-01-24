3cc6428bf8494bef6ddc78536a92bd20f4fd5661
azarovalex <azarov.333@gmail.com>
2018-01-24 03:12:59 +0300

    Layout and UI elements for side menu added.

---
+++ azChat-iOS/View/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  azChat-iOS
+//
+//  Created by Alex Azarov on 24/01/2018.
+//  Copyright © 2018 Alex Azarov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
