15d8b6fb9fea3793883c43d7c30324721f8999d8
azarovalex <azarov.333@gmail.com>
2018-01-25 17:43:52 +0300

    Login View added.

---
+++ azChat-iOS/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  azChat-iOS
+//
+//  Created by Alex Azarov on 25/01/2018.
+//  Copyright © 2018 Alex Azarov. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtnClicked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ azChat-iOS/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  azChat-iOS
+//
+//  Created by Alex Azarov on 25/01/2018.
+//  Copyright © 2018 Alex Azarov. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
